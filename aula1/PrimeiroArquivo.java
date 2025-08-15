6960a1aad6c625ab8b06b4b92fb2fbc6f6f90011
Gustavo Micher Santana <gustavomicher10@gmail.com>
2025-08-15 01:32:15 +0000

    aula1

---
+++ aula1/PrimeiroArquivo.java
@@ -0,0 +1,6 @@
+public class PrimeiroArquivo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Programação de Sistemas I");
+    }
+}
